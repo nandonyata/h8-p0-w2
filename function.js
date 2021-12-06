8707b69695e852884a1cbeff6e75dbbcb4f6ad90
nandonyata <nandonp02@gmail.com>
2021-12-07 03:59:25 +0700

    menambahkan file2 js

---
+++ function.js
@@ -0,0 +1,19 @@
+// soal 1
+function ShoutOut(name = 'Function') {
+  console.log(`Halo ${name}!`);
+}
+console.log(ShoutOut());
+
+// soal 2
+function kali(num1, num2) {
+  return num1 * num2;
+}
+
+kali(5, 6);
+
+// soal 3
+function fullSentence(name, age, address, hobby) {
+  console.log(`Nama saya ${name}, umumr saya ${age}, alamat saya di ${address}, dan saya punya hobby yaitu ${hobby}`);
+}
+
+fullSentence('nando, 20, simpangHaru, main');
